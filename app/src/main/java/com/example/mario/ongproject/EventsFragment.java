334ed792fe3ed7a523e684ffb23d94723d757d87
Mario Apra <mario_surfing@hotmail.com>
2017-05-12 16:30:10 -0300

    Add events layout

--- app/src/main/java/com/example/mario/ongproject/EventsFragment.java
+++ app/src/main/java/com/example/mario/ongproject/EventsFragment.java
@@ -1,10 +1,30 @@
 package com.example.mario.ongproject;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 /**
  * Created by mario on 05/05/17.
  */
 
 public class EventsFragment extends Fragment {
+    CalendarView calendar;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.events_view, container, false);
+
+        calendar = (CalendarView) v.findViewById(R.id.calendarView);
+
+        return v;
+    }
+
+    public void populateCalendar(){
+
+    }
 }
--- app/src/main/java/com/example/mario/ongproject/PatronizeFragment.java
+++ app/src/main/java/com/example/mario/ongproject/PatronizeFragment.java
@@ -1,10 +1,24 @@
 package com.example.mario.ongproject;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by mario on 05/05/17.
  */
 
 public class PatronizeFragment  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.patronize_view, container, false);
+
+
+        return v;
+    }
 }
