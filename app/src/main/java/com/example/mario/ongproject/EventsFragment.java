15883190314d1610c0c26aab550ae8704f4bfa31
derrix060 <mario_surfing@hotmail.com>
2017-05-14 19:25:31 -0300

    add custom Calendar

--- app/src/main/java/com/example/mario/ongproject/EventsFragment.java
+++ app/src/main/java/com/example/mario/ongproject/EventsFragment.java
@@ -1,30 +1,76 @@
 package com.example.mario.ongproject;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
+
+import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by mario on 05/05/17.
  */
 
 public class EventsFragment extends Fragment {
-    CalendarView calendar;
+
+    CaldroidFragment caldroidFragment;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.events_view, container, false);
 
-        calendar = (CalendarView) v.findViewById(R.id.calendarView);
+
+        caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+
+        FragmentTransaction t = getFragmentManager().beginTransaction();
+        t.replace(R.id.customCalendar, caldroidFragment);
+        t.commit();
+
+        populateCalendar();
 
         return v;
     }
 
     public void populateCalendar(){
+        ColorDrawable green = new ColorDrawable(Color.GRAY);
+        Calendar myCal = Calendar.getInstance();
 
+        myCal.set(Calendar.YEAR, 2017);
+        myCal.set(Calendar.MONTH, 4);
+        myCal.set(Calendar.DAY_OF_MONTH, 20);
+        Date dt = myCal.getTime();
+
+        caldroidFragment.setBackgroundDrawableForDate(green, dt);
+
+        myCal.set(Calendar.YEAR, 2017);
+        myCal.set(Calendar.MONTH, 4);
+        myCal.set(Calendar.DAY_OF_MONTH, 22);
+        dt = myCal.getTime();
+
+        caldroidFragment.setBackgroundDrawableForDate(green, dt);
+
+        /*
+        05/20/2017 - Pizza Night
+        05/22/2017 - Bingo Night
+
+
+         */
+
+
+        caldroidFragment.refreshView();
     }
 }
